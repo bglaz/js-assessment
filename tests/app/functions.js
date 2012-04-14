f1f2a67e423989661513aefafa5e028f7833ddff
Brian Glaz <brian.glaz@gmail.com>
2012-04-14 12:32:13 -0400

    finished tests in Function section

--- tests/app/functions.js
+++ tests/app/functions.js
@@ -6,6 +6,11 @@ define([ 'use!underscore' ], function(_) {
         fn = function() {};
 
     it("you should be able to use an array as arguments when calling a function", function() {
+      
+      var fn = function(args) {
+      	return sayIt.apply(this,args);
+      }
+      
       var result = fn([ 'Hello', 'Ellie', '!' ]);
       expect(result).to.be('Hello, Ellie!');
     });
@@ -19,18 +24,29 @@ define([ 'use!underscore' ], function(_) {
             name : 'Rebecca'
           };
 
-      // define a function for fn that calls the speak function such that the
+      var fn = function() {
+      	return speak.apply(obj);
+      }
+      
       // following test will pass
       expect(fn()).to.be('Hello, Rebecca!!!');
     });
 
     it("you should be able to return a function from a function", function() {
-      // define a function for fn so that the following will pass
+      
+      var fn = function(arg) {
+      	return function(arg2) { return arg + ', ' + arg2}
+      }
+      
       expect(fn('Hello')('world')).to.be('Hello, world');
     });
 
     it("you should be able to create a 'partial' function", function() {
-      // define a function for fn so that the following will pass
+      var fn = function(fnc,arg1,arg2) {
+      	return function(arg3) {
+      		return fnc(arg1,arg2,arg3);
+      	}
+      }
       var partial = fn(sayIt, 'Hello', 'Ellie');
       expect(partial('!!!')).to.be('Hello, Ellie!!!');
     });
