fd0b46f0bbfa3e279dc4dfea34df60e0ea7b4192
Brian Glaz <brian.glaz@gmail.com>
2012-04-14 11:36:56 -0400

    finished tests in Array section.

--- tests/app/arrays.js
+++ tests/app/arrays.js
@@ -9,35 +9,49 @@ define([ 'use!underscore' ], function(_) {
         baz : 'bim'
       };
 
-      fn = function() { };
+       fn = function(){}
     });
 
     it("you should be able to determine the location of an item in an array", function() {
-      // define a function for fn so that the following will pass
+      fn = function(arr,i) {return a.indexOf(i) };
       expect(fn(a, 3)).to.be(2);
     });
 
     it("you should be able to add the values of an array", function() {
-      // define a function for fn so that the following will pass
+      	fn = function(arr) {
+      		var a = 0;
+      		for(var i = arr.length; i--;) {
+      			a += arr[i];
+      		}
+      		return a;
+      	}
       expect(fn(a)).to.be(10);
     });
 
     it("you should be able to remove an item from an array", function() {
-      // define a function for fn so that the following will pass
+      	fn = function(arr,el) {
+      		var res = arr.splice(arr.indexOf(el),1);
+      		return arr;
+      	}
       var result = fn(a, 2);
       expect(result).to.have.length(3);
       expect(result.join(' ')).to.be('1 3 4');
     });
 
     it("you should be able to add an item to the end of an array", function() {
-      // define a function for fn so that the following will pass
+      fn = function(arr,el) {
+      	arr.push(el);
+      	return arr;
+      }
       var result = fn(a, 10);
       expect(result).to.have.length(5);
       expect(result[result.length - 1]).to.be(10);
     });
 
     it("you should be able to create an array from two arrays", function() {
-      // define a function for fn so that the following will pass
+      fn = function(arr1,arr2) {
+      	return arr1.concat(arr2)
+      }
       var c = [ 'a', 'b', 'c' ],
           result = fn(a, c);
 
@@ -46,7 +60,10 @@ define([ 'use!underscore' ], function(_) {
     });
 
     it("you should be able to add an item anywhere in an array", function() {
-      // define a function for fn so that the following will pass
+      fn = function(arr,el,index) {
+      	arr.splice(index,0,el);
+      	return arr;
+      }
       var result = fn(a, 'z', 2);
 
       expect(result).to.have.length(5);
